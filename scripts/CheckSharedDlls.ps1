8480d66388289c7a487117528810cbba8ea41bbb
niltor <zpty@outlook.com>
2025-07-29 19:04:54 +0800

    调整打包脚本

--- scripts/CheckSharedDlls.ps1
+++ scripts/CheckSharedDlls.ps1
@@ -12,4 +12,38 @@ dotnet publish  (Join-Path $commandLineDir "CommandLine.csproj") -c Release -o (
 dotnet publish  (Join-Path $studioDir "AterStudio.csproj") -c Release -o (Join-Path $studioDir "publish")
 
 
-dotnet run ./CheckSharedDlls.cs
+## 检查共享的 DLL 文件
+$path1 = "../src/Command/CommandLine/publish"
+$path2 = "../src/Services/AterStudio/publish"
+
+# 获取所有 .dll 文件的文件名（不含路径），转为 HashSet（通过 [System.Collections.Generic.HashSet[string]] 实现）
+$files1 = Get-ChildItem -Path $path1 -Recurse -Filter *.dll | Select-Object -ExpandProperty Name
+$files2 = Get-ChildItem -Path $path2 -Recurse -Filter *.dll | Select-Object -ExpandProperty Name
+
+# 转换为 HashSet 以便高效交集
+$set1 = [System.Collections.Generic.HashSet[string]]::new()
+$set2 = [System.Collections.Generic.HashSet[string]]::new()
+$files1 | ForEach-Object { $set1.Add($_) | Out-Null }
+$files2 | ForEach-Object { $set2.Add($_) | Out-Null }
+
+# 求交集
+$shareDlls = $set1.Where({ $set2.Contains($_) })
+
+# 输出结果
+Write-Host "Total common DLL files: $($shareDlls.Count)"
+
+## 保存到文件
+$shareDllsFile = Join-Path $location "ShareDlls.txt"
+$shareDlls | Out-File -FilePath $shareDllsFile -Encoding UTF8
+
+# common的总大小
+$totalSize = 0
+$shareDlls | ForEach-Object {
+    $filePath1 = Join-Path $path1 $_
+    
+    if (Test-Path $filePath1) {
+        $totalSize += (Get-Item $filePath1).Length
+    }
+}
+Write-Host "Total size of common DLL files: $([Math]::Round($totalSize / 1MB, 2)) MB"
+
--- scripts/PublishToLocal.ps1
+++ scripts/PublishToLocal.ps1
@@ -59,76 +59,21 @@ try {
         
         dotnet publish -c release -o ./publish -p:GenerateDocumentationFile=false -p:DebugType=None
         # 移除部分 dll文件，减少体积
+        # 读取ShareDlls.txt，获取dll列表
+        $shareDllsFile = Join-Path $location "ShareDlls.txt"
+        if (Test-Path $shareDllsFile) {
+            $shareDlls = Get-Content -Path $shareDllsFile
+            foreach ($dll in $shareDlls) {
+                $dllPath = ".\publish\$dll"
+                if (Test-Path $dllPath) {
+                    Remove-Item -Path $dllPath -Force
+                }
+            }
+        }
         $pathsToRemove = @(
             ".\publish\BuildHost-net472",
             ".\publish\BuildHost-netcore",
             ".\publish\runtimes",
-
-            ".\publish\Ater.Common.dll"
-            ".\publish\Ater.Web.Convention.dll"
-            ".\publish\CodeGenerator.dll"
-            ".\publish\Entity.dll"
-            ".\publish\EntityFramework.dll"
-            ".\publish\Humanizer.dll"
-            ".\publish\Mapster.Core.dll"
-            ".\publish\Mapster.dll"
-            ".\publish\Microsoft.AspNetCore.Razor.Language.dll"
-            ".\publish\Microsoft.Build.dll"
-            ".\publish\Microsoft.Build.Framework.dll"
-            ".\publish\Microsoft.Build.Locator.dll"
-            ".\publish\Microsoft.Build.Tasks.Core.dll"
-            ".\publish\Microsoft.Build.Utilities.Core.dll"
-            ".\publish\Microsoft.CodeAnalysis.CSharp.dll"
-            ".\publish\Microsoft.CodeAnalysis.CSharp.Workspaces.dll"
-            ".\publish\Microsoft.CodeAnalysis.dll"
-            ".\publish\Microsoft.CodeAnalysis.ExternalAccess.RazorCompiler.dll"
-            ".\publish\Microsoft.CodeAnalysis.Workspaces.dll"
-            ".\publish\Microsoft.CodeAnalysis.Workspaces.MSBuild.dll"
-            ".\publish\Microsoft.Data.Sqlite.dll"
-            ".\publish\Microsoft.EntityFrameworkCore.Abstractions.dll"
-            ".\publish\Microsoft.EntityFrameworkCore.dll"
-            ".\publish\Microsoft.EntityFrameworkCore.Relational.dll"
-            ".\publish\Microsoft.EntityFrameworkCore.Sqlite.dll"
-            ".\publish\Microsoft.Extensions.DependencyModel.dll"
-            ".\publish\Microsoft.IdentityModel.Abstractions.dll"
-            ".\publish\Microsoft.IdentityModel.JsonWebTokens.dll"
-            ".\publish\Microsoft.IdentityModel.Logging.dll"
-            ".\publish\Microsoft.IdentityModel.Tokens.dll"
-            ".\publish\Microsoft.NET.StringTools.dll"
-            ".\publish\Microsoft.OpenApi.dll"
-            ".\publish\Microsoft.OpenApi.Readers.dll"
-            ".\publish\Microsoft.VisualStudio.Setup.Configuration.Interop.dll"
-            ".\publish\Newtonsoft.Json.dll"
-            ".\publish\RazorEngineCore.dll"
-            ".\publish\Share.dll"
-            ".\publish\SharpYaml.dll"
-            ".\publish\Spectre.Console.dll"
-            ".\publish\SQLitePCLRaw.batteries_v2.dll"
-            ".\publish\SQLitePCLRaw.core.dll"
-            ".\publish\SQLitePCLRaw.provider.e_sqlite3.dll"
-            ".\publish\System.CodeDom.dll"
-            ".\publish\System.Composition.AttributedModel.dll"
-            ".\publish\System.Composition.Convention.dll"
-            ".\publish\System.Composition.Hosting.dll"
-            ".\publish\System.Composition.Runtime.dll"
-            ".\publish\System.Composition.TypedParts.dll"
-            ".\publish\System.Configuration.ConfigurationManager.dll"
-            ".\publish\System.IdentityModel.Tokens.Jwt.dll"
-            ".\publish\System.Reflection.MetadataLoadContext.dll"
-            ".\publish\System.Resources.Extensions.dll"
-            ".\publish\System.Security.Cryptography.ProtectedData.dll"
-            ".\publish\System.Security.Permissions.dll"
-            ".\publish\System.Windows.Extensions.dll"
-            ".\publish\Microsoft.IO.Redist.dll"
-            ".\publish\System.Buffers.dll"
-            ".\publish\System.Collections.Immutable.dll"
-            ".\publish\System.CommandLine.dll"
-            ".\publish\System.Memory.dll"
-            ".\publish\System.Numerics.Vectors.dll"
-            ".\publish\System.Runtime.CompilerServices.Unsafe.dll"
-            ".\publish\System.Threading.Tasks.Extensions.dll"
-            ".\publish\Microsoft.CodeAnalysis.Workspaces.MSBuild.BuildHost.dll"
-            ".\publish\Share.resources.dll" 
             ".\publish\AterStudio.exe",
             ".\publish\openapi_admin.json"
         );
